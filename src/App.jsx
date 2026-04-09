6e8c6ee2ae01e21781acc4ef2510e3dc68d31441
Jael Nantamu <jaellynda6@gmail.com>
2026-04-09 12:12:45 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Landing from './pages/Landing';
 import GetMyID from './pages/GetMyID';
 import Dashboard from './pages/Dashboard';
